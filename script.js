24816f7e8d4c5274510195ded91ad9848b26a3ee
MrMadnessj <shivarama.off2@gmail.com>
2023-07-10 23:43:18 +0530

    remade the whole js

--- script.js
+++ script.js
@@ -1,67 +1,101 @@
-function getComputerChoice(){
-    let choices = ['rock', 'paper', 'scissor'];
-    let choice = choices[Math.floor(Math.random() * choices.length)];
-    return choice;
+let selectionButtons = document.querySelectorAll('[id]');
+const finalColumn = document.querySelector('.choices');
+let elem;
+
+const yourScoreSpan = document.querySelector('[data-your-score]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
+let body = document.body;
+
+
+
+const SELECTIONS = [
+    {
+        name:'rock',
+        beats:'scissor'
+    },
+    {
+        name:'paper',
+        beats:'rock'
+    },
+    {
+        name:'scissor',
+        beats:'paper'
+    }
+];
+
+
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.id;
+        const selection = SELECTIONS.find(selection => selection.name === selectionName);
+        makeSelection(selection);
+
+        endgame();
+    })
+})
+
+
+function makeSelection(selection){
+    const computerSelection = randomSelection()
+    console.log("selection: ", selection);
+    console.log("computer choice: ", computerSelection)
+    const yourWinner = isWinner(selection, computerSelection);
+    const computerWinner = isWinner(computerSelection, selection);
+    console.log(computerWinner);
+    console.log(yourWinner);
+
+    addSelectionResult(computerSelection,computerWinner);
+
+
+    if(yourWinner) incrementScore(yourScoreSpan);
+    if(computerWinner) incrementScore(computerScoreSpan);
+
+
+
 }
 
-function playGame(playerChoice, computerChoice){
-    let result = '';
-    //playerChoice = playerChoice.toLowerCase();
-    console.log('player choice: '+playerChoice);
-    console.log('computer choice: '+computerChoice);
+function incrementScore(scoreSpan){
+    scoreSpan.innerText = parseInt(scoreSpan.innerText)+1;
+}
 
-    if(playerChoice == computerChoice)
-        result = 'Draw the match'
+function addSelectionResult(selection,winner){
+    elem = document.createElement('div');
+    elem.innerText = `Computer Chose ${selection.name}`;
+    if(winner) elem.style.cssText = "color: #db0404";
+    else elem.style.cssText = "color: green";
+    finalColumn.appendChild(elem);
+}
 
-    else if(playerChoice == 'rock')
-    {
-        if(computerChoice == 'paper')
-            result = `You Lose, ${computerChoice} beats ${playerChoice}`;
-        else
-            result = `You Win, ${playerChoice} beats ${computerChoice}`;
-    }   
-
-    else if(playerChoice == 'paper')
-    {
-        if(computerChoice == 'scissor')
-            result = `You Lose, ${computerChoice} beats ${playerChoice}`;
-        else
-            result = `You Win, ${playerChoice} beats ${computerChoice}`;
-    } 
-
-    else if(playerChoice == 'scissor')
-    {
-        if(computerChoice == 'rock')
-            result = `You Lose, ${computerChoice} beats ${playerChoice}`;
-        else
-            result = `You Win, ${playerChoice} beats ${computerChoice}`;
-    } 
-
-    return result;
+function endgame(){
+    if(yourScoreSpan.innerText == '5'){
+        selectionButtons.disabled = true;
+        body.style.cssText = 'background-color: #52f54c';
+        elem.style.cssText = "color: green";
+        elem.innerText = "Aight you win!";
+    }
+    if(computerScoreSpan.innerText == '5'){
+        selectionButtons.disables = true;
+        body.style.cssText = 'background-color: #780800';
+        elem.style.cssText = "color: red";
+        elem.innerText = "See you later Alligator :]"
+    }    
 }
 
 
-//const plrchc = document.querySelector('.btn-sel');
+ 
 
-let pc = 'rock';
-function game(pc){
-    let cc = getComputerChoice();
-    console.log(playGame(pc,cc));
+function isWinner(selection, opponentSelection){
+    return selection.beats === opponentSelection.name;
 }
 
-// let plrChoice = prompt('Enter rock,paper or scissor');
+
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+    return SELECTIONS[randomIndex];
+}
 
 
-const buttons = document.querySelectorAll(".btn-sel");
 
-let plrChoice;
-buttons.forEach(button => button.addEventListener("click",event => {
-    plrChoice = event.currentTarget.id;
-    return plrChoice
-}));
-
-game(plrChoice);
-console.log(pc);
 
 
 
